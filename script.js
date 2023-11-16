99efd36e2f607fcf3719385dfac5e230f389d03a
LuVernon <lucasvernon@icloud.com>
2023-11-16 09:18:54 -0800

    Operate function.

--- script.js
+++ script.js
@@ -17,3 +17,12 @@ function multiply (a, b) {
 function divide (a, b) {
     return a/b;
 }
+
+function operate (first, second, oper) {
+    switch (oper) {
+        case "+": add(first, second);
+        case "-": subtract(first, second);
+        case "*": multiply(first, second);
+        case "/": divide(first, second);
+    }
+}
