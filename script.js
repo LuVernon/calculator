4389cf0c6ab1d1ad375514705f3e0f800ceca275
LuVernon <lucasvernon@icloud.com>
2023-11-16 09:15:56 -0800

    Linked script and css to HTML file. Added basic add, subtract, multiply, divide functions.

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+function add (a, b) {
+    return a+b;
+}
+
+function subtract (a, b) {
+    return a-b
+}
+
+function multiply (a, b) {
+    return a*b;
+}
+
+function divide (a, b) {
+    return a/b;
+}
